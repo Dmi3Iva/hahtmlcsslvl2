1ad6583436641ba2efd7cc7a52de2d8f93751c0e
DmitriyIvanov <Diklacway@gmail.com>
2019-08-03 13:27:17 +0200

    fix slider

--- source/js/main.js
+++ source/js/main.js
@@ -14,76 +14,79 @@ window.onload  = function(){
   
   /*cat slider before after*/
   //for mobile
-  if(window.innerWidth <768){
-    
-    document.querySelector('.presentation__checkbox-input').addEventListener('click',function(){
-      document.querySelector('.slider-cat__item:first-child').classList.toggle('slider-cat__item--close');
-    });
-    
-  }
-  else{
-    //desktop
-    let line        = document.querySelector('.presentation__line');
-    let button      = document.querySelector('.presentation__button');
-    let before      = document.querySelector('.slider-cat__item:first-child');
-    let after       = document.querySelector('.slider-cat__item:last-child');  
-    let parentWidth = document.querySelector('.slider-cat__list').offsetWidth;
-    let inputResult = document.querySelector('.presentation__checkbox-input');
-    
-    
-    line.addEventListener('click',function(e){
-      let rect = line.getBoundingClientRect();
-      button.style.left = (e.clientX-rect.left) + 'px';
-      changeWidth((e.clientX-rect.left) / (rect.right - rect.left));
-    });
-    
-    function changeWidth(value){
-      before.style.width = value * parentWidth + 'px';  
-    }    
-    
-    changeWidth(0.5);
-    button.onmousedown = buttonMove;
-    
-    button.ondragstart = function() {
-      return false;
-    };
-    
-    function buttonMove(event){
-      let btn = this;
-      let leftPosition = getPositionX(btn.parentElement);
-      moveAt(event.pageX);
-      
-      function moveAt(pageX){
-        let pos = pageX - leftPosition - btn.offsetWidth /2;
-        
-        if(pos < 0 ){
-          pos = 0;
-        }
-        if(pos > btn.parentElement.clientWidth-12){
-          pos = btn.parentElement.clientWidth-12;
-        }
-        
-        btn.style.left =  pos + 'px';
-        
-        inputResult.value = pos/(btn.parentElement.clientWidth-12);
-        changeWidth(inputResult.value); 
-      }
-      
-      function onMouseMove(event) {
-        moveAt(event.pageX);
-      }
-
-      document.addEventListener('mousemove', onMouseMove);
-
-      document.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        this.onmouseup = null;
-      };
+  // document.querySelector('.slider-cat__control--mobile').addEventListener('click',function(){
+  //   document.querySelector('.slider-cat__item:first-child').classList.toggle('slider-cat__item--close');
+  // });
+  
+  //for desktop
+  let line        = document.querySelector('.slider-cat__line');
+  let button      = document.querySelector('.slider-cat__button');
+  let before      = document.querySelector('.slider-cat__first');
+  let after       = document.querySelector('.slider-cat__second');  
+  
+  line.addEventListener('click',function(e){
+    let rect = line.getBoundingClientRect();
+    let pos =  ( e.clientX - rect.left - 17 ) ;
+    if( pos < 0 ){
+      pos = 0;
     }
+    if(pos > button.parentElement.clientWidth - 34 ){
+      pos = button.parentElement.clientWidth  - 34;
+    }
+    button.style.left = pos + 'px';
+    changeWidth( pos / ( rect.right - rect.left - 34) );
+  });
+  
+  function changeWidth(value){
+    before.style.width = value * 100 + '%';
+    after.style.width = 100 - (value * 100) + '%';    
+  }    
+  
+  changeWidth(0.5);
+  
+  button.onmousedown = buttonMove;
+  
+  button.ondragstart = function() {
+    return false;
+  };
+  
+  function buttonMove(event){
+    let leftPosition = getPositionX(button.parentElement);
+    event.preventDefault();
+    
+    function moveAt(pageX){
+      let pos = pageX - leftPosition - button.offsetWidth / 2;
+      
+      if(pos < 0 ){
+        pos = 0;
+      }
+      if(pos > button.parentElement.clientWidth - 34){
+        pos = button.parentElement.clientWidth - 34;
+      }
+      
+      button.style.left =  pos + 'px';
+      console.log(pos);
+      console.log(button.parentElement.clientWidth);
+      changeWidth(pos/ (button.parentElement.clientWidth -34));
+    }
+    
+    function onMouseMove(event) {
+      moveAt(event.pageX);
+      console.log('pagex' + event.pageX);
+    }
+
+    document.addEventListener("mousemove",  onMouseMove);
+    
+    document.onmouseup = function() {
+      console.log('stop');
+      document.removeEventListener('mousemove', onMouseMove);
+      document.onmouseup = null;
+    };
   }
 
 }//end window onload
 
+/*functions*/
 function getPositionX(elem){
   return elem.getBoundingClientRect().left ;
 }
